ac78b24e121cd143b22d1d4971ec6eb377c8b320
Soren Hansen <sorhanse@cisco.com>
2012-04-23 22:36:31 +0200

    Ignore resolvconf when choosing name servers

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,14 @@ class dnsmasq($only_dns = true) {
         ensure => present
     }
 
+    file { "/etc/default/dnsmasq":
+        content => template("dnsmasq/dnsmasq.default.erb"),
+        owner => root,
+        group => root,
+        mode => 0644,
+        notify => Exec["reload-dnsmasq"]
+    }
+
     file { "/etc/dnsmasq.conf":
         content => template("dnsmasq/dnsmasq.conf.erb"),
         owner => root,
